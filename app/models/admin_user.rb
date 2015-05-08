d74479af1e65c2976e5a7feda626ee2db8d9f710
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2015-05-08 19:55:08 +0300

    View and Model Updates
    View and Model Updates

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,4 +1,5 @@
 class AdminUser < ActiveRecord::Base
   has_many :section_edits
   has_many :sections, :through => :section_edits
+  has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+
   # get 'demo/index'
 
   match ':controller(/:action(/:id))', :via => :get
